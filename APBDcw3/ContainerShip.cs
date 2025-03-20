dbedbda51504fa1482e2613ec4da1c2ed6a68f78
granek-ux <j.balinski@wp.pl>
2025-03-20 19:46:05 +0100

    Shpi

---
+++ APBDcw3/ContainerShip.cs
@@ -0,0 +1,12 @@
+﻿namespace APBDcw3;
+
+public class ContainerShip
+{
+    private List<Container> containers { get; set; }
+    private double maxSpeed { get; set; }
+    private int maxCointaners { get; set; }
+    private double maxWeightOfContainer { get; set; }
+    
+    
+
+}
