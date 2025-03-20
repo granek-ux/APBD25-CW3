b2c4fc3a83b61091eac2ff3e1608ea497b56cfd2
granek-ux <j.balinski@wp.pl>
2025-03-20 16:56:19 +0100

    Contener abstact v1

---
+++ APBDcw3/Contener.cs
@@ -0,0 +1,26 @@
+﻿namespace APBDcw3;
+
+public abstract class Contener
+{
+    private int weightOfLoad { get; set; }
+
+    private int height { get; set; }
+
+    private int weightOfContener { get; set; }
+
+    private String serialNumber { get; set; }
+    
+    private int maxLoadWeight { get; set; }
+
+    public void emptyContener()
+    {
+        weightOfLoad = 0;
+    }
+
+    public void LoadContener(int weight)
+    {
+        if (weight + weightOfLoad> maxLoadWeight)
+            throw new OverfillException("too much weight");
+        weightOfLoad += weight;
+    }
+}
