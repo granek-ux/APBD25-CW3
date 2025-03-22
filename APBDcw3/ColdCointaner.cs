4809859c3131f0809a2335fa589d1530dddf7dc2
granek-ux <j.balinski@wp.pl>
2025-03-22 14:12:03 +0100

    map

--- APBDcw3/ColdCointaner.cs
+++ APBDcw3/ColdCointaner.cs
@@ -2,14 +2,31 @@
 
 public class ColdCointaner : Container, IHazardNotifer
 {
+    private string typeOfProduct { get; set; }
+    double temperature { get; set; }
+    private Dictionary<Products, double> productDictionary;
     public ColdCointaner( double weightOfLoad, double height, double weightOfContener, double maxLoadWeight, string typeOfProduct, double temperature) : base("C", weightOfLoad, height, weightOfContener, maxLoadWeight)
     {
         this.typeOfProduct = typeOfProduct;
         this.temperature = temperature;
+        this.productDictionary = new Dictionary<Products, double>();
+        
+        this.productDictionary = new Dictionary<Products, double>
+        {
+            { Products.Banana, 13.3 },
+            { Products.Chocolate, 18 },
+            {Products.Fish, 2 },
+            { Products.Meat, -15 },
+            { Products.Ice_Cream, -18 },
+            { Products.Frozen_Pizza, -30 },
+            { Products.Cheese, 7.2 },
+            { Products.Sausage, 5 },
+            { Products.Butter, 20.5 },
+            { Products.Eggs, 19 }
+        };
+        
     }
 
-    private string typeOfProduct { get; set; }
-    double temperature { get; set; }
     
 
     public void NotifyHazard()
